8dd4b07e77a7c2af78e851d5de965667514563d5
Max White <mushishi78@gmail.com>
2015-02-07 17:33:17 +0000

    Remove active support dependency

--- end_view.gemspec
+++ end_view.gemspec
@@ -15,5 +15,4 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'rspec', '~> 3.1', '>= 3.1.0'
   s.add_development_dependency 'haml'
   s.add_development_dependency 'rspec-html-matchers'
-  s.add_development_dependency 'activesupport'
 end
--- lib/end_view/rails_layout.rb
+++ lib/end_view/rails_layout.rb
@@ -1,11 +1,12 @@
 require 'end_view'
 require 'haml'
 require 'tilt/haml'
-require 'active_support/core_ext/module/delegation'
+require 'forwardable'
 
 module EndView
   class RailsLayout
     include EndView.new(__FILE__, Tilt::HamlTemplate)
+    extend Forwardable
 
     def initialize(view_context, title)
       @view_context, @title = view_context, title
@@ -13,10 +14,10 @@ module EndView
 
     private
 
-    attr_reader :view_context
-    delegate :stylesheet_link_tag,
-             :javascript_include_tag,
-             :csrf_meta_tags, to: :view_context
+    attr_reader :view_context, :title
+    def_delegators :view_context, :stylesheet_link_tag,
+                                  :javascript_include_tag,
+                                  :csrf_meta_tags
 
     def stylesheet_args
       ['application', { media: 'all', 'data-turbolinks-track' => true }]
@@ -33,7 +34,7 @@ __END__
 !!! 5
 %html
   %head
-    %title= @title
+    %title= title
     = stylesheet_link_tag(*stylesheet_args)
     = javascript_include_tag(*javascript_args)
     = csrf_meta_tags
